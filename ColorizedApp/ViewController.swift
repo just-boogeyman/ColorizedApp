c5bf62b57d6ec270ab45ab9f9053bc55ed457822
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-09-12 22:05:10 +0700

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Ярослав Кочкин on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
